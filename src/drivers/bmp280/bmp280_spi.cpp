119ac1b2e723a3907d4751e1769c2993803ccbf7
cosmicog <orhangazi4401@gmail.com>
2018-12-20 17:03:24 +0300

    Rename param

--- src/drivers/bmp280/bmp280_spi.cpp
+++ src/drivers/bmp280/bmp280_spi.cpp
@@ -51,94 +51,102 @@
 #if defined(PX4_SPIDEV_BARO) || defined(PX4_SPIDEV_EXT_BARO)
 
 #pragma pack(push,1)
-struct spi_data_s {
-	uint8_t addr;
-	struct bmp280::data_s data;
+struct spi_data_s
+{
+    uint8_t addr;
+    struct bmp280::data_s data;
 };
 
-struct spi_calibration_s {
-	uint8_t addr;
-	struct bmp280::calibration_s cal;
+struct spi_calibration_s
+{
+    uint8_t addr;
+    struct bmp280::calibration_s cal;
 };
 #pragma pack(pop)
 
 class BMP280_SPI: public device::SPI, public bmp280::IBMP280
 {
 public:
-	BMP280_SPI(uint8_t bus, uint32_t device, bool external);
-	virtual ~BMP280_SPI() = default;
+    BMP280_SPI(uint8_t bus, uint32_t device, bool is_external_device);
+    virtual ~BMP280_SPI() = default;
 
-	bool is_external();
-	int init();
+    bool is_external();
+    int init();
 
-	uint8_t get_reg(uint8_t addr);
-	int set_reg(uint8_t value, uint8_t addr);
-	bmp280::data_s *get_data(uint8_t addr);
-	bmp280::calibration_s *get_calibration(uint8_t addr);
+    uint8_t get_reg(uint8_t addr);
+    int set_reg(uint8_t value, uint8_t addr);
+    bmp280::data_s * get_data(uint8_t addr);
+    bmp280::calibration_s * get_calibration(uint8_t addr);
 
 private:
-	spi_calibration_s _cal;
-	spi_data_s _data;
-	bool _external;
+    spi_calibration_s _cal;
+    spi_data_s _data;
+    bool _external;
 };
 
-bmp280::IBMP280 *bmp280_spi_interface(uint8_t busnum, uint8_t device, bool external)
+bmp280::IBMP280 * bmp280_spi_interface(uint8_t busnum, uint8_t device, bool external)
 {
-	return new BMP280_SPI(busnum, device, external);
+    return new BMP280_SPI(busnum, device, external);
 }
 
-BMP280_SPI::BMP280_SPI(uint8_t bus, uint32_t device, bool external) :
-	SPI("BMP280_SPI", nullptr, bus, device, SPIDEV_MODE3, 10 * 1000 * 1000)
+BMP280_SPI::BMP280_SPI(uint8_t bus, uint32_t device, bool is_external_device) :
+    SPI("BMP280_SPI", nullptr, bus, device, SPIDEV_MODE3, 10 * 1000 * 1000)
 {
-	_external = external;
+    _external = is_external_device;
 }
 
 bool BMP280_SPI::is_external()
 {
-	return _external;
+    return _external;
 };
 
 int BMP280_SPI::init()
 {
-	return SPI::init();
+    return SPI::init();
 };
 
 uint8_t BMP280_SPI::get_reg(uint8_t addr)
 {
-	uint8_t cmd[2] = { (uint8_t)(addr | DIR_READ), 0}; //set MSB bit
-	transfer(&cmd[0], &cmd[0], 2);
+    uint8_t cmd[2] = { (uint8_t)(addr | DIR_READ), 0}; //set MSB bit
+    transfer(&cmd[0], &cmd[0], 2);
 
-	return cmd[1];
+    return cmd[1];
 }
 
 int BMP280_SPI::set_reg(uint8_t value, uint8_t addr)
 {
-	uint8_t cmd[2] = { (uint8_t)(addr & DIR_WRITE), value}; //clear MSB bit
-	return transfer(&cmd[0], nullptr, 2);
+    uint8_t cmd[2] = { (uint8_t)(addr & DIR_WRITE), value}; //clear MSB bit
+    return transfer(&cmd[0], nullptr, 2);
 }
 
-bmp280::data_s *BMP280_SPI::get_data(uint8_t addr)
+bmp280::data_s * BMP280_SPI::get_data(uint8_t addr)
 {
-	_data.addr = (uint8_t)(addr | DIR_READ); //set MSB bit
+    _data.addr = (uint8_t)(addr | DIR_READ); //set MSB bit
 
-	if (transfer((uint8_t *)&_data, (uint8_t *)&_data, sizeof(struct spi_data_s)) == OK) {
-		return &(_data.data);
+    if (transfer((uint8_t *)&_data, (uint8_t *)&_data, sizeof(struct spi_data_s)) == OK)
+    {
+        return &(_data.data);
 
-	} else {
-		return nullptr;
-	}
+    }
+    else
+    {
+        return nullptr;
+    }
 }
 
-bmp280::calibration_s *BMP280_SPI::get_calibration(uint8_t addr)
+bmp280::calibration_s * BMP280_SPI::get_calibration(uint8_t addr)
 {
-	_cal.addr = addr | DIR_READ;
+    _cal.addr = addr | DIR_READ;
 
-	if (transfer((uint8_t *)&_cal, (uint8_t *)&_cal, sizeof(struct spi_calibration_s)) == OK) {
-		return &(_cal.cal);
+    if (transfer((uint8_t *)&_cal, (uint8_t *)&_cal, sizeof(struct spi_calibration_s)) == OK)
+    {
+        return &(_cal.cal);
 
-	} else {
-		return nullptr;
-	}
+    }
+    else
+    {
+        return nullptr;
+    }
 }
 
 #endif /* PX4_SPIDEV_BARO || PX4_SPIDEV_EXT_BARO */
